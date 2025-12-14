06740d4fccaa0563c2e51f1c01a313aa85e66d28
davis kanters <61993593+cyanisntblue@users.noreply.github.com>
2025-12-14 12:20:08 -0800

    groupby update
    use more thorough grouping to ensure songs with the same name are not added together

--- diyspotifywrapped.py
+++ diyspotifywrapped.py
@@ -6,7 +6,7 @@ from datetime import datetime
 def main():
     directory = autoDetectDirectory()
     if directory == False:
-        directory = getDirectory(greenInput('Enter the "Spotify Extended Streaming History" Directory > '))
+        directory = removeQuotes(greenInput('Enter the "Spotify Extended Streaming History" Directory > '))
     else:
         print("Path Auto-Detected")
 
@@ -16,7 +16,7 @@ def main():
     #df = pd.read_csv('spotify_data.csv')
     #df['ts'] = pd.to_datetime(df['ts'])
 
-    years = list({i for i in df['ts'].dt.year})
+    years = list({i for i in df['ts'].dt.year}) # retrieve all available years
 
     selectyear = True
 
@@ -24,6 +24,7 @@ def main():
 
         # year selection loop
         while selectyear == True:
+
             year = greenInput(f"From which year would you like data? ({min(df['ts']).year}-{max(df['ts']).year}) > ")
 
             if year.lower() == 'quit':
@@ -39,10 +40,12 @@ def main():
             
         selectyear = False
 
+
+        # START choice selection loop
         choice = greenInput(f"What would you like to see? (Artists, Songs, Albums) type quit to exit > ")
+
         if choice.lower() == 'quit':
             exit()
-
         elif choice.lower() == 'artist' or choice.lower() == 'artists' or choice.lower() == 'ar':
             count = greenInput("How many to display? (default 5) > ")
 
@@ -57,11 +60,11 @@ def main():
         elif choice.lower() == 'song' or choice.lower() == 'songs' or choice.lower() == 's':
             count = greenInput("How many to display? (default 5) > ")
 
-            data = yearFilter(df, year).groupby('master_metadata_track_name')[['sec_played']].sum().sort_values('sec_played',ascending=False).head(numToDisplay(count))
+            data = yearFilter(df, year).groupby(['master_metadata_track_name', 'master_metadata_album_artist_name', 'spotify_track_uri'])[['sec_played']].sum().sort_values('sec_played',ascending=False).head(numToDisplay(count))
             print(f"\nTop songs of {year}:")
 
             for i, song in enumerate(data.index):
-                print(f"{i+1}. {song}")
+                print(f"{i+1}. {song[0]} - {song[1]}") # [0] is song name, [1] is artist name, [2] is spotify id
             print()
             selectyear = True
 
@@ -77,11 +80,12 @@ def main():
             selectyear = True
         else:
             print("choice not recognized, please try again")
+        # END choice selection loop
 
 
 
 def autoDetectDirectory():
-    # auto-detect if this program is ran inside the target folder
+    # auto-detect if this program is ran INSIDE the target folder
     if os.path.dirname(os.path.realpath(__file__)).endswith("Spotify Extended Streaming History"):
         directory = os.path.dirname(os.path.realpath(__file__))
         return directory
@@ -89,7 +93,7 @@ def autoDetectDirectory():
     else:
         return False
 
-def getDirectory(directory):
+def removeQuotes(directory):
     # remove single quotes around directory (if drag and dropped to terminal)
     if directory.startswith("'") and directory.endswith("'"):
         directory = directory[1:-1]
@@ -123,13 +127,14 @@ def yearFilter(data, year):
         return (data[(data['ts'] >= datetime(int(year),1,1)) & (data['ts'] <= datetime(int(year),12,31))]).sort_values('ts')
 
 def numToDisplay(count):
+    # number of songs/artists/albums to display (defaults to 5 if input invalid)
     if count.isdigit():
         pass
     else:
         count = 5
     return int(count)
 
-def greenInput(msg):
+def greenInput(msg): # input() but green!
     print(msg + "\033[92m", end="")
     userinput = input()
     print("\033[0m", end="")
